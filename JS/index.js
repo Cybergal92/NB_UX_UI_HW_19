dcc3f4e0c5cd2ee078f046b08d764e4e6b4d38d1
Cybergal92 <136454599+Cybergal92@users.noreply.github.com>
2023-07-13 18:31:47 +1000

    working on java

--- JS/index.js
+++ JS/index.js
@@ -1,17 +1,13 @@
-console.log("Your index.js file is loaded correctly!");
-
-$("#myID").on("click", function() {
-    console.log("You clicked on #myID");
-});
-
-(document).ready(function() {
-      // Attach event listener to the image
-      $(".enlarge").hover(
-        function() {
-          $(this).addClass("enlarge"); // Add the enlarge class on hover
-        },
-        function() {
-          $(this).removeClass("enlarge"); // Remove the enlarge class on mouse leave
-        }
-      );
+$(document).ready(function() {
+    console.log("Your index.js file is loaded correctly!");
+  
+    $("#myID").on("hover", function() {
+      console.log("You clicked on #myID");
     });
+  
+    $(".IMAGE").on("hover",
+      function() {
+        $(this).addClass("enlarge"); // Add the enlarge class on hover
+      }
+    );
+  });

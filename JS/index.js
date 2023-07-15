827adba9868368397d122b44d6f7135b1369aa3a
Cybergal92 <136454599+Cybergal92@users.noreply.github.com>
2023-07-15 12:48:01 +1000

    accordion set up

--- JS/index.js
+++ JS/index.js
@@ -1,13 +1,25 @@
-$(document).ready(function() {
-    console.log("Your index.js file is loaded correctly!");
+console.log("Your index.js file is loaded correctly!");
   
     $("#myID").on("hover", function() {
       console.log("You clicked on #myID");
     });
-  
-    $(".IMAGE").on("hover",
-      function() {
-        $(this).addClass("enlarge"); // Add the enlarge class on hover
+    
+  // Get all accordion headers
+  const accordionHeaders = document.querySelectorAll('.accordion-header');
+
+  // Attach click event listeners to each accordion header
+  accordionHeaders.forEach(header => {
+    header.addEventListener('click', function() {
+      // Toggle active class on the clicked accordion
+      this.parentElement.classList.toggle('active');
+
+      // Toggle display of accordion content
+      const content = this.nextElementSibling;
+      if (content.style.display === 'block') {
+        content.style.display = 'none';
+      } else {
+        content.style.display = 'block';
       }
-    );
+    });
   });
+ 
